cb456a46238f2b8f92a0b41e3d1c2531b0ca84b2
BadLice <alerandom99@gmail.com>
2021-10-14 18:49:33 +0200

    using NN to resolve xor to test if it works

--- sketch.js
+++ sketch.js
@@ -1,13 +1,36 @@
 let brain;
+let trainingData = [
+	{
+		inputs: [0, 0],
+		targets: [0],
+	},
+	{
+		inputs: [1, 1],
+		targets: [0],
+	},
+	{
+		inputs: [1, 0],
+		targets: [1],
+	},
+	{
+		inputs: [0, 1],
+		targets: [1],
+	},
+];
 
 function setup() {
-	brain = new NeuralNetwork(2, 2, 3);
+	//neural network test -> solve xor function -> 2 inputs, 2 hidden, 1 output
+	brain = new NeuralNetwork(2, 2, 1);
 
-	let inputs = [1, 0];
-	let targets = [1, -1, 1];
+	for (let i = 0; i < 1000000; i++) {
+		const { inputs, targets } = random(trainingData);
+		brain.train(inputs, targets);
+	}
 
-	let output = brain.train(inputs, targets);
-	console.log(output);
+	console.log(brain.feedforward([0, 0]));
+	console.log(brain.feedforward([1, 0]));
+	console.log(brain.feedforward([0, 1]));
+	console.log(brain.feedforward([1, 1]));
 }
 
 function draw() {}
